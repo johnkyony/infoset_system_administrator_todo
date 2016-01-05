1d51c2fca6c6bcb14ee6cb77df66188faf01e61e
korujamscongo <korujamscongo@gmail.com>
2016-01-05 09:32:29 +0000

    admin password set up

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.new({name: 'admin'  , email: 'admin@admin.com', password: 'starwars15'} )
+admin.save
