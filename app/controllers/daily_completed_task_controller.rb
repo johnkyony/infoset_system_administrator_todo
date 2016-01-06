6d8e5a1f68f818867b766866c8cf65290ead1bc4
korujamscongo <korujamscongo@gmail.com>
2016-01-06 14:15:24 +0000

    The order list order

--- app/controllers/daily_completed_task_controller.rb
+++ app/controllers/daily_completed_task_controller.rb
@@ -1,7 +1,7 @@
 class DailyCompletedTaskController < ApplicationController
   def index
     complete = Task.where.not(completed: nil).pluck(:id)
-    @completed = Task.where(id: complete)
+    @completed = Task.where(id: complete).order(completed: :desc)
         
   end
 end
