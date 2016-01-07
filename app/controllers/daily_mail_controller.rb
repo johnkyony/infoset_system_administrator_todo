20d6e8d1374c1fcd3da97b2479c9bbdc0f772684
korujamscongo <korujamscongo@gmail.com>
2016-01-07 16:23:15 +0000

    Email set up done

--- app/controllers/daily_mail_controller.rb
+++ app/controllers/daily_mail_controller.rb
@@ -2,10 +2,18 @@ class DailyMailController < ApplicationController
     
     def index
         require 'gmail'
-        @pending = Task.where(completed: nil)
-        complete = Task.where.not(completed: nil).pluck(:id)
-        @completed = Task.where(id: complete).order(completed: :desc)
-        
+        pending = Task.where(completed: nil)
+        # complete = Task.where.not(completed: nil).pluck(:id)
+        completed = Task.find_by(completed: Date.today)
+        pending_body_content = ""
+        pending.each do |i|
+            pending_body_content += " - #{i.title}  \n"
+        end
+        completed_body_content = ""
+        completed.each do |c|
+            completed_body_content += "- #{c.title} \n"
+  
+        end
         user_name = 'kyony.j@infoset.cd'
         password  = 'f@cus1sbr1ght21'
         gmail = Gmail.new(user_name,password) 
@@ -13,10 +21,7 @@ class DailyMailController < ApplicationController
             to "jkyony@gmail.com"
             subject "Daily report"
             text_part do 
-                body "This are the following task that are completed"
-                   
-                    
-                    
+                body "This are the following task that are completed \n #{completed_body_content} \n This is the pending task \n #{pending_body_content} " 
             end
         end
         
